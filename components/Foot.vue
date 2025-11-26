e4380b0065b3688ba23e56678088902cb17831af
octi14 <gonzalezoctavio5@gmail.com>
2025-11-26 20:36:32 -0300

    fix vercel

--- components/Foot.vue
+++ components/Foot.vue
@@ -5,7 +5,7 @@
         <div class="mx-auto" style="text-align:center">
           <p>2025 - Municipalidad de Villa Gesell  </p>
           <!-- <p> <b-icon-telephone scale="0.9"/> Sec. Hacienda: (02255) 478073</p> -->
-          <p> <small> Sitio desarrollado por el Depto. Modernización del Estado - <a class="text-light" href="https://arvige.gob.ar" target="blank">ARVIGE  </a></small></p>
+          <p> <small>Sitio desarrollado por el Depto. Modernización del Estado - <a class="text-light" href="https://arvige.gob.ar" target="blank">ARVIGE  </a></small></p>
         </div>
       </div>
     </div>
